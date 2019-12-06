724e3797a1a463380f4a8c366525c709c03413b1
Galina <galina@Olegs-MacBook-Pro.local>
2019-12-06 15:48:54 +0300

    Component styling basics

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <footer className="footer">This is the footer</footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/MainContent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./MainContent.css"
+
+function MainContent() {
+  return (
+    <main className="main">This is main section</main>
+  );
+};
+
+export default MainContent;
